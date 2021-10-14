c33a33a2df84b07f51ed1fdd487173ab0c83c410
Kyohan <smith.khalyl@gmail.com>
2021-10-14 13:46:00 -0500

    user model created. added django-choices to requirements for django enums

--- project/reservations/models.py
+++ project/reservations/models.py
@@ -1,3 +1,19 @@
+from django import forms
 from django.db import models
+from django.forms.widgets import PasswordInput
 
-# Create your models here.
+from djchoices import ChoiceItem, DjangoChoices
+
+class PaymentMethod(DjangoChoices):
+    CASH = ChoiceItem('Cash')
+    CREDIT = ChoiceItem('Credit')
+    CHECK = ChoiceItem('Check')
+
+class RegisteredUser(models.Model):
+    name = models.CharField(max_length = 30)
+    email = models.EmailField(primary_key = True)
+    password = forms.CharField(widget = PasswordInput)
+    mailing_address = models.TextField()
+    billing_address = models.TextField()
+    diner_no = models.IntegerField(null = True)
+    payment_method = models.CharField(max_length = 20, choices = PaymentMethod.choices)
--- project/reservations/test/test_user.py
+++ project/reservations/test/test_user.py
@@ -2,8 +2,7 @@ from django.test import TestCase
 
 from reservations.models import *
 
-# Create your tests here.
 class UserModelTest(TestCase):
 
-  def test_canary(self):
-    self.assertTrue(True)
+    def test_canary(self):
+        self.assertTrue(True)
