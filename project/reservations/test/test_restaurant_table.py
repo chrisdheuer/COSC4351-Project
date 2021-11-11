5320fd9f457c80aa31a72b1bcc852aee8b637993
Kyohan <smith.khalyl@gmail.com>
2021-11-10 20:46:25 -0600

    cleanup

--- project/reservations/test/test_restaurant_table.py
+++ project/reservations/test/test_restaurant_table.py
@@ -1,32 +1,34 @@
 from django.test import TestCase
-from reservations.models import *
+from reservations.models import RestaurantTable
 from django.core.exceptions import ValidationError
 
-def create_table(capacity):
-    return RestaurantTable(capacity = capacity)
-
 class RestaurantTableModelTest(TestCase):
+    
+    def create_table(self, capacity):
+        return RestaurantTable(capacity = capacity)
     
     def test_canary(self):
         self.assertTrue(True)
 
     def test_initializing_a_table(self):
-        table = create_table(4)
+        table = self.create_table(4)
         table.save()
 
         self.assertEqual(RestaurantTable.objects.count(), 1)
         
     def test_table_capacity_limits(self):
-        table_one = create_table(2)
+        table_one = self.create_table(2)
         table_one.save()
         self.assertEqual(RestaurantTable.objects.count(), 1)
+        self.assertLessEqual(table_one.capacity, 8)
 
-        table_two = create_table(8)
+        table_two = self.create_table(8)
         table_two.save()
         self.assertEqual(RestaurantTable.objects.count(), 2)
+        self.assertLessEqual(table_two.capacity, 8)
 
-        table_three = create_table(7)
-        table_four = create_table(10)
+        table_three = self.create_table(7)
+        table_four = self.create_table(10)
         
         self.assertRaises(ValidationError, table_three.save)
         self.assertRaises(ValidationError, table_four.save)
