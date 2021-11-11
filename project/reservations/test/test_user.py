2842059bbe54c87a1bf4b98a144250cb52432ec3
Kyohan <smith.khalyl@gmail.com>
2021-11-10 20:47:01 -0600

    cleanup and two more tests

--- project/reservations/test/test_user.py
+++ project/reservations/test/test_user.py
@@ -1,29 +1,48 @@
 from django.contrib.auth import get_user_model
 from django.test import TestCase
-from reservations.models import *
+from django.core.exceptions import ValidationError
 
 class UserModelTest(TestCase):
 
     def setUp(self):
         self.User = get_user_model()
+        
+    def create_a_user(self, first_name = 'John', last_name = 'Smith', email = 'test123@example.com', password = 'pass', mailing_address = '123 N Avenue', billing_address = '123 N Avenue'):
+        return self.User.objects.create_user(
+            first_name = first_name,
+            last_name = last_name,
+            email = email,
+            password = password,
+            mailing_address = mailing_address,
+            billing_address = billing_address,
+        )
 
     def test_canary(self):
         self.assertTrue(True)
 
     def test_creating_a_user(self):
-        user = self.User.objects.create_user(
-            first_name = 'John',
-            last_name = 'Smith',
-            email = 'test123@example.com',
-            password = 'pass',
-            mailing_address = '123 N Avenue',
-            billing_address = '456 S Street',
-            diner_no = 1,
-            payment_method = PaymentMethod.CASH,
-        )
+        user = self.create_a_user()
         
         self.assertTrue(user.is_active)
         self.assertFalse(user.is_staff)
         self.assertFalse(user.is_superuser)
 
         self.assertEqual(1, self.User.objects.count())
+        
+    def test_user_password_is_encrypted_upon_creation(self):
+        user = self.create_a_user()
+        
+        self.assertNotEqual(user.password, 'pass')
+    
+    def test_return_error_creating_a_user_with_empty_fields(self):
+        user = self.create_a_user(
+            first_name = '',
+            last_name = '',
+            email = '',
+            password = '',
+            mailing_address = '',
+            billing_address = ''
+        )
+        
+        self.assertRaises(ValidationError, user.clean)
+    
