6d5e5907e586cf9d8b6f846f0961d8ac192e2f0e
Kyohan <smith.khalyl@gmail.com>
2021-11-21 20:44:46 -0600

    still might need some work but added check for user at index view

--- project/reservations/views.py
+++ project/reservations/views.py
@@ -1,4 +1,8 @@
 from django.shortcuts import render
+from .models import RegisteredUser
 
-def index(request):
-  return render(request, 'reservations/index.html')
+def index(request, user_id = None):
+  user = RegisteredUser.objects.get(id = user_id) if user_id else None
+  
+  return render(request, 'reservations/index.html', {'user': user})
+  
