88938017976ddcaee064acef38ae6d1642ce9037
Kyohan <smith.khalyl@gmail.com>
2021-11-21 20:44:14 -0600

    added django-bootstrap-icons module to requirements

--- project/project/settings.py
+++ project/project/settings.py
@@ -37,7 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    
     'reservations',
+    
+    'django_bootstrap_icons',
 ]
 
 MIDDLEWARE = [
